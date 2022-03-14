c324618ea463d256eddd60cf8be785bfa8a1817f
Gayathri123-4 <gayurey0606@gmail.com>
2022-03-14 11:27:34 +0530

    new codes

---
+++ src/main/java/org/bb/Sample.java
@@ -0,0 +1,12 @@
+package org.bb;
+
+import org.testng.annotations.Test;
+
+public class Sample {
+@Test
+private void test1() {
+System.out.println("Test A1");
+System.out.println("Test A2");
+System.out.println("Test A3");
+}
+}
